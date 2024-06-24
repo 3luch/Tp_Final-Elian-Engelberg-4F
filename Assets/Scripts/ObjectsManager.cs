2d4fbe38afbd2111e6fca870c44cc1b69268b003
Nicodry20 <48178309@est.ort.edu.ar>
2024-06-24 15:56:27 -0300

    Listo

---
+++ Assets/Scripts/ObjectsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsManager : MonoBehaviour
+{
+    public GameObject[] objects;
+    int currentIndex = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeactivateAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentIndex++;
+            if (currentIndex >= objects.Length)
+            {
+                currentIndex = 0;
+            }
+            DeactivateAll();
+            ActivateByIndex(currentIndex);
+        }
+    }
+
+    void DeactivateAll()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i].SetActive(false);
+        }
+    }
+    void ActivateByIndex(int index)
+    {
+        objects[index].SetActive(true);
+    }
+}
